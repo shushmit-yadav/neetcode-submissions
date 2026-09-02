9ecbf5c0dfd480052190c959462ae9367366f075
Shushmit Yadav <shushmit.yadav02@gmail.com>
2026-09-02 20:22:16 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @param {string} word
+     * @return {boolean}
+     */
+    exist(board, word) {
+        const ROWS = board.length, COLS = board[0].length;
+        const path = new Set();
+
+        function backtrack(r, c, i){
+            if(i === word.length) return true;
+            if(
+                r < 0 || 
+                c < 0 || 
+                r >= ROWS || 
+                c >= COLS || 
+                board[r][c] !== word[i] || 
+                path.has(`${r},${c}`)) {
+                return false;
+            }
+
+            path.add(`${r},${c}`);
+
+            const res = backtrack(r+1,c,i+1) || backtrack(r-1, c, i+1) || backtrack(r, c+1,i+1) || backtrack(r,c-1,i+1);
+
+            path.delete(`${r},${c}`);
+
+            return res;
+        }
+
+        for(let r = 0; r < ROWS; r++){
+            for(let c = 0; c < COLS;  c++){
+                if (backtrack(r,c, 0)) return true;
+            }
+        }
+
+        return false;
+    }
+}
